fcf6f4686c3012ef56ec094675d846b51ff08c7c
Christopher Imantaka <chris.imantaka1997@gmail.com>
2020-05-08 22:12:49 -0700

    First Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+
+def main():
+    '''
+    This is the main fucntion where the code going to be called
+    all loads and dumps happens here
+
+    :return: None
+    '''
+
+    print("Hello World")
