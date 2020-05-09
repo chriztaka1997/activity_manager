bdeba104392c2df9581fadb04c83b74d7967d0ca
Christopher Imantaka <chris.imantaka1997@gmail.com>
2020-05-09 16:48:54 -0700

    Json load is done, moving to printing_all function

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import json
 from os import path
+import time
+
+
+def printing_all(long_term, targets, today_activity):
+    '''
+    printing all current today related stuff
+
+    :param long_term: set
+    :param targets: set
+    :param today_activity: set
+    :return: none
+    '''
+    print("Long Term Targets: ")
+    for i in long_term:
+        print()
+
 
 
 def main():
@@ -18,30 +34,27 @@ def main():
     weekly_summary_path = "./Database/weekly_summary.json"
 
     ## This is loading the database ###
-    if (path.exists(long_term_path)): long_term_target = json.loads(open(long_term_path))
+    if (path.exists(long_term_path)): long_term_target = json.loads(open(long_term_path,"r"))
     else: long_term_target = {}
 
-    if (path.exists(targets_path)): targets = json.loads(open(targets_path))
+    if (path.exists(targets_path)): targets = json.loads(open(targets_path,"r"))
     else: targets = {}
 
-    if (path.exists(today_activity_path)): today_activity = json.loads(open(today_activity_path))
+    if (path.exists(today_activity_path)): today_activity = json.loads(open(today_activity_path,"r"))
     else: today_activity = {}
 
-    if(path.exists(summary_path)): summary = json.loads(open(summary_path))
+    if(path.exists(summary_path)): summary = json.loads(open(summary_path,"r"))
     else: summary = {}
 
-    if (path.exists(weekly_summary_path)): weekly_summary = json.loads(open(weekly_summary_path))
+    if (path.exists(weekly_summary_path)): weekly_summary = json.loads(open(weekly_summary_path,"r"))
     else: weekly_summary = {}
 
+    # Ask for user input
+    # assuming the user input is always valid
 
 
-
-
-    print("Hello World")
-    print("Testing shortcut")
-    for x in long_term_target:
-        print(str(x)+". "+long_term_target[x])
 
     # TODO: json dumps
 
-main()
+# main()
+print(time.localtime())
