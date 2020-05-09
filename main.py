1f31e8a6bbd25f4c9fb3ef7a720f661dc552b375
Christopher Imantaka <chris.imantaka1997@gmail.com>
2020-05-08 22:31:53 -0700

    Testing json loads

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ def main():
 
     :return: None
     '''
+    # TODO: json loads
+
+    try:
+        long_term_target = json.loads(open("./Database/long_term_target.json"))
+    except:
+        long_term_target = {1:"test", 2:"test2"}
+        print("Error in loading")
+
 
     print("Hello World")
     print("Testing shortcut")
+    for x in long_term_target:
+        print(str(x)+". "+long_term_target[x])
+
+    # TODO: json dumps
+
+main()
